a27a40c877ee0ca5040af556affad8dafe05e70a
gusslik <grigory.bocharsky@gmail.com>
2025-03-05 19:26:31 +0000

    Added GUI using SFML

--- bouncing_ball_simulation/main.cpp
+++ bouncing_ball_simulation/main.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <SFML/Graphics.hpp>
 
 // gravitational constant
-const float g = -9.81f;
+const float g = -1000.f;
 
 // coefficient of restitution
 const float e = 0.5f;
 
-// time interval between iterations of the simulation (1 ms)
-const float timeInterval = 0.001f;
-
-struct Vector2D {
-	float x;
-	float y;
-};
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
 
 struct Ball {
-	Vector2D position;
-	Vector2D velocity;
+	sf::Vector2f position;
+	sf::Vector2f velocity;
 };
 
 // Update the velocity of the ball (delta v = u + at)
-Vector2D update_velocity(Vector2D vel) {
-	float newVelY = vel.y + g * timeInterval;
+sf::Vector2f update_velocity(sf::Vector2f vel, sf::Time timeElapsed) {
+	float newVelY = vel.y + g * timeElapsed.asSeconds();
 	
 	return {vel.x, newVelY};
 }
 
 // Update the position of the ball (delta y = ut + 0.5ut^2)
-Vector2D update_position(Ball ball) {
-	float posX = ball.position.x + ball.position.x * timeInterval;
-	float posY = ball.position.y + (ball.velocity.y * timeInterval + 0.5 * g * timeInterval * timeInterval);
+sf::Vector2f update_position(Ball ball, sf::Time timeElapsed) {
+	float posX = ball.position.x + ball.velocity.x * timeElapsed.asSeconds();
+	float posY = ball.position.y + (ball.velocity.y * timeElapsed.asSeconds() + 0.5 * g * timeElapsed.asSeconds() * timeElapsed.asSeconds());
 
 	return { posX, posY };
 }
@@ -47,27 +43,53 @@ Ball detect_collision(Ball ball) {
 
 int main() {
 	// Initial Position
-	Vector2D iPos{ 0, 2 };
+	sf::Vector2f iPos{ 0, SCREEN_HEIGHT-100 };
 	
 	// Initial Velocity
-	Vector2D iVel{ 10, 2 };
+	sf::Vector2f iVel{ 200, 0 };
 
 	// Instance of the ball
 	Ball ball{ iPos, iVel };
-	
-	// main simulation loop
-	while (1) {
+
+	sf::Clock clock;
+	sf::RenderWindow window(sf::VideoMode({ SCREEN_WIDTH, SCREEN_HEIGHT }), "Hello World");
+	window.setFramerateLimit(60);
+
+	sf::View view;
+	view.setSize({ SCREEN_WIDTH, -SCREEN_HEIGHT});
+	view.setCenter({ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 });
+	window.setView(view);
+
+	sf::CircleShape circle(50);
+	circle.setFillColor(sf::Color::Blue);
+
+	while (window.isOpen()) {
+		sf::Time elapsed = clock.restart();
+		
+		while (const std::optional event = window.pollEvent()) {
+
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
+
 		// Updates the velocity of the ball after 1 ms
-		Vector2D vel = update_velocity(ball.velocity);
+		sf::Vector2f vel = update_velocity(ball.velocity, elapsed);
 
 		// Updates the position of the ball after 1 ms
-		Vector2D pos = update_position(ball);
+		sf::Vector2f pos = update_position(ball, elapsed);
 
 		// Check if the ball went below y-axis and assign a new value for the velocity and position of the ball
 		ball = detect_collision({ pos, vel });
 
-		// Waits for 1 ms before the start of a new cycle
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		circle.setPosition(ball.position);
+
+		window.clear(sf::Color::White);
+
+		window.draw(circle);
+
+		window.display();
+
 	}
 
 	return 0;
